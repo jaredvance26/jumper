37cda034545b0fe4d97a5c37fd537fe204ee3d7a
Brandon Densmore <densmoreb@Brandons-MacBook-Pro.local>
2021-05-19 15:14:45 -0600

    created word file

---
+++ jumper_template/jumper/word.py
@@ -0,0 +1,29 @@
+import random
+
+class Word:
+    def __init__(self):
+        self.words = ['light', 'apple', 'characterization', 'table']
+
+    def get_word(self):
+        self.rand_word = random.choice(self.words)
+        self.guessed = []
+        print(self.rand_word)
+        for char in self.rand_word:
+            self.guessed.append('_')
+
+    def char_num(self):
+        self.num = len(self.rand_word)
+
+        return self.num
+
+    def compare_letter(self, guess):
+        x = 0
+        success = False
+        for i in self.rand_word:
+            if guess == i:
+                self.guessed[x] = guess
+                success = True
+            x += 1
+        print((self.guessed))
+        return success
+
---
+++ jumper_template/test.py
@@ -0,0 +1,11 @@
+from jumper.word import Word
+
+word = Word()
+word.get_word()
+
+word.compare_letter('a')
+word.compare_letter('p')
+
+print(word.compare_letter('p'))
+
+
