d06e5f2bf71e1fdb58f28cd2804e8dcc362806a2
jared vance <jaredvance@205-185-96-174.directcom.com>
2021-05-19 16:50:17 -0600

    added to console and director

---
+++ jumper_template/jumper/game/console.py
@@ -0,0 +1,14 @@
+class Console:
+    """
+    Shows input and output.
+
+    stereotype: Service provider
+    
+    """
+
+    def write(message):
+        """
+        Gets the message and shows it to the user.
+        """
+
+        return(input(message))
